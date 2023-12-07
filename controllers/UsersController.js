72ac8e3c1369bca2d50371a2c20e831aab0deb76
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-12-07 05:33:36 +0300

    Task 2

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,66 +1,59 @@
+/* eslint-disable */
+import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
 const sha1 = require('sha1');
-const { MongoClient } = require('mongodb');
 
-let db;
-
-async function connectToDatabase() {
-  const client = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+// POST /users should create a new user in DB
+export async function postNew(req, res) {
 
   try {
-    await client.connect();
-    console.log('Connected to the database');
-    db = client.db('your_database_name'); // Replace 'your_database_name' with your actual database name
+    const userEmail = req.body.email;
+    if (!userEmail) {
+      return res.status(400).send({
+        error: 'Missing email',
+      });
+    }
+
+    const userPassword = req.body.password;
+    if (!userPassword) {
+      return res.status(400).send({
+        error: 'Missing password',
+      });
+    }
+    
+    let existingEmail = await dbClient.db.collection('users').findOne({ email: userEmail });
+    if (existingEmail) {
+      return res.status(400).send({
+        error: 'Already exist',
+      });
+    }
+
+    let userId;
+    const hashedPw = sha1(userPassword);
+    const newUser = {
+      email: userEmail,
+      password: hashedPw,
+    };
+
+    try {
+      await dbClient.db.collection('users').insertOne(newUser, (err) => {
+        userId = newUser._id;
+        return res.status(201).send({
+          email: userEmail,
+          id: userId,
+        });
+      });
+    } catch (err) {
+      return res.status(err.status).send({
+        'error': err,
+      });
+    }
+
   } catch (error) {
-    console.error('Error connecting to the database', error);
-    throw error;
+    return res.status(500).send({
+      error: 'Server error',
+    });
   }
 }
-
-const UsersController = {
-  postNew: async (req, res) => {
-    try {
-      if (!db) {
-        await connectToDatabase();
-      }
-
-      const { email, password } = req.body;
-
-      // Check if email and password are provided
-      if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-      }
-
-      if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-      }
-
-      const users = db.collection('users');
-
-      // Check if email already exists
-      const existingUser = await users.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ error: 'Already exists' });
-      }
-
-      // Hash the password (you might want to use a better hashing library)
-      const hashedPassword = sha1(password);
-
-      // Create a new user
-      const newUser = {
-        email,
-        password: hashedPassword,
-      };
-
-      // Save the user to the collection
-      const result = await users.insertOne(newUser);
-
-      // Return the new user details
-      return res.status(201).json({ id: result.insertedId, email });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
-};
-
-module.exports = UsersController;
